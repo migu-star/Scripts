79fb98e0cbc6d491c54cdbb36ddf34acd25de1a6
MiguDEV <71216796+migu-star@users.noreply.github.com>
2021-10-17 20:11:30 +0200

    Added auto-egg & fixed bugs

--- Pet-Legends-Candy.lua
+++ Pet-Legends-Candy.lua
@@ -1,5 +1,25 @@
+local PreciosEggs = require(game:GetService("ReplicatedStorage").GameInfo.EggsCost)
+
+local selectedcurrency = "Money"
+local TipoDinero = game:GetService("Players").LocalPlayer.PlayerData.Currency[selectedcurrency].Value
 local PetsEquipadas = game:GetService("Players").LocalPlayer.PlayerData.EquippedPets:GetChildren()
 local PetsFarmeando = 0
+
+-- Currency table
+TiposDeDinero = {}
+
+for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerData.Currency:GetChildren()) do
+    table.insert(TiposDeDinero,v.Name)
+end
+-- Pets table
+
+Eggs = {}
+for i,v in pairs(game:GetService("Workspace").Eggs:GetChildren()) do
+    if string.find(v.Name,"Egg") then
+        table.insert(Eggs,v.Name)
+    end
+end
+local selectedegg = "Nothing"
 
 local candymade = 0 
 local startcandy = game:GetService("Players").LocalPlayer.PlayerData.Currency.Candy.Value
@@ -26,9 +46,23 @@ AutoFarm:addToggle("Farm candy", true, function(value)
 end)
 
 
--- Only for add
+local Miscsec = venyx:addPage("Misc", 5012544693)
+local EggsSec = Miscsec:addSection("Eggs")
 
--- second page
+EggsSec:addDropdown("Select currency", TiposDeDinero, function(Tipo)
+    selectedcurrency = Tipo
+end)
+
+EggsSec:addDropdown("Select egg", Eggs, function(Huevo)
+    print("Selected", huevo)
+    selectedegg = Huevo
+end)
+
+local AutoEgg = false
+EggsSec:addToggle("Auto open egg", false, function(value)
+    AutoEgg = value
+end)
+-- settings page
 local settingssec = venyx:addPage("Settings", 5012544693)
 local colors = settingssec:addSection("Colors")
 local others = settingssec:addSection("Others")
@@ -51,12 +85,16 @@ end
 
 others:addButton("Destroy script", function()
 	farmingcandy = false
+	AutoEgg = false
 	game:GetService("CoreGui"):FindFirstChild("StarScriptsX - Pet legends - Multi pet in beta"):Destroy()
+	script:Destroy()
 end)
 venyx:SelectPage(venyx.pages[1], true)
 print("Loaded")
 
 while true do wait()
+    TipoDinero = game:GetService("Players").LocalPlayer.PlayerData.Currency[selectedcurrency].Value
+    PetsEquipadas = game:GetService("Players").LocalPlayer.PlayerData.EquippedPets:GetChildren()
     if farmingcandy then
         if not game:GetService("Workspace").TargetsMarkers.Kreis:FindFirstChild("CandyChest") then
             local PetsFarmeando = 0
@@ -64,11 +102,15 @@ while true do wait()
             repeat
                 game:GetService("ReplicatedStorage").Events.GiveTarget:InvokeServer(workspace.Chest:WaitForChild("CandyChest"))
                 PetsFarmeando = PetsFarmeando + 1
-                print(PetsFarmeando)
-                print(PetsEquipadas)
-            until PetsFarmeando == #PetsEquipadas
+            until PetsFarmeando >= #PetsEquipadas
         end
     end
     candymade = game:GetService("Players").LocalPlayer.PlayerData.Currency.Candy.Value - startcandy
     info:updateButton(candymadebutton, "Candy made: ".. candymade)
+    if AutoEgg then
+        if TipoDinero >= tonumber(PreciosEggs[selectedegg]) then
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Workspace").Eggs[selectedegg].CFrame
+            game:GetService("ReplicatedStorage").Events.BuyEgg:InvokeServer(selectedegg,1)
+        end
+    end
 end
