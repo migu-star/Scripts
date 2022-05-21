fcafb727984266178a8af9bda904be16e4d61907
MiguDEV <71216796+migu-star@users.noreply.github.com>
2022-05-22 01:57:21 +0200

    Made kick more cool

--- ElectricStateX.lua
+++ ElectricStateX.lua
@@ -1,41 +1,37 @@
-    setclipboard("discord.gg/RTDtEY9drb")
-    if syn then
-        syn.request({
-           Url = "http://127.0.0.1:6463/rpc?v=1",
-           Method = "POST",
-           Headers = {
-               ["Content-Type"] = "application/json",
-               ["Origin"] = "https://discord.com"
+setclipboard("https://discord.gg/rGdF4Y9uza")
+if syn then
+    syn.request({
+       Url = "http://127.0.0.1:6463/rpc?v=1",
+       Method = "POST",
+       Headers = {
+           ["Content-Type"] = "application/json",
+           ["Origin"] = "https://discord.com"
+       },
+       Body = game:GetService("HttpService"):JSONEncode({
+           cmd = "INVITE_BROWSER",
+           args = {
+               code = "rGdF4Y9uza"
            },
-           Body = game:GetService("HttpService"):JSONEncode({
-               cmd = "INVITE_BROWSER",
-               args = {
-                   code = "RTDtEY9drb"
-               },
-               nonce = game:GetService("HttpService"):GenerateGUID(false)
-           }),
-        })
+           nonce = game:GetService("HttpService"):GenerateGUID(false)
+       }),
+    })
         
-    elseif request then
-        request({
-            Url = 'http://127.0.0.1:6463/rpc?v=1',
-            Method = 'POST', -- <optional> | GET/POST/HEAD, etc.
-            Headers = {
-                ["Content-Type"] = "application/json",
-                ["Origin"] = "https://discord.com"
+elseif request then
+    request({
+        Url = 'http://127.0.0.1:6463/rpc?v=1',
+        Method = 'POST', -- <optional> | GET/POST/HEAD, etc.
+        Headers = {
+            ["Content-Type"] = "application/json",
+            ["Origin"] = "https://discord.com"
+        },
+        Body = game:GetService("HttpService"):JSONEncode({
+            cmd = "INVITE_BROWSER",
+            args = {
+                code = "rGdF4Y9uza"
             },
-            Body = game:GetService("HttpService"):JSONEncode({
-                cmd = "INVITE_BROWSER",
-                args = {
-                    code = "rGdF4Y9uza"
-                },
-                nonce = game:GetService("HttpService"):GenerateGUID(false)
-            }),
-        })
-    end
+            nonce = game:GetService("HttpService"):GenerateGUID(false)
+        }),
+    })
+end
 
-game.Players.LocalPlayer:kick([[
-  Project Meteor / StarScriptsX
-  The new discord invite has been copied to clipboard.
-  
-  Join it for get new script.]])
+game.Players.LocalPlayer:kick("\nProject Meteor / StarScriptsX\n\nThe new discord invite has been copied to clipboard.\n\nJoin it for get new script.")
